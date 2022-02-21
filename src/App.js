ad0d6b10f0834f0ffe0c404f938154af4769b026
akshithP <akshithp13@gmail.com>
2022-02-21 15:19:00 +1100

    Initial layout and file structure setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Routes } from "./components/Routes";
 
 const App = () => {
-  return (
-    <div>
-        <h2>App</h2>
-    </div>
-  )
-}
+  // Option to set dark/light mode
+  const [darkMode, setDarkMode] = useState(false);
 
-export default App
+  return (
+    <div className={darkMode ? "dark" : "light"}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
