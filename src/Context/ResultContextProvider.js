ba54247b97d0cc059bd03abb49ffab7a8c6c2c3f
akshithP <akshithp13@gmail.com>
2022-02-26 00:08:28 +1100

    Completed all major functionality

--- src/Context/ResultContextProvider.js
+++ src/Context/ResultContextProvider.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React, { useState, useContext, createContext } from "react";
 
 // useContext hook to fetch results from API and share it across
@@ -9,7 +10,7 @@ const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Ronaldo");
 
   // Async fetch to make API calls
   const getResults = async (resultType) => {
@@ -18,8 +19,33 @@ export const ResultContextProvider = ({ children }) => {
     const response = await fetch(`${baseUrl}${resultType}`, {
       method: "GET",
       headers: {
-       
+        "x-user-agent": "desktop",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY
       },
     });
+
+    const data = await response.json();
+
+    if (resultType.includes("/news")) {
+      setResults(data.entries);
+    } else if (resultType.includes("/images")) {
+      setResults(data.image_results);
+    } else {
+      setResults(data.results);
+    }
+
+    setLoading(false);
   };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
 };
+
+// Makes it easier to use values from this context by exporting the function
+export const useResultContext = () => useContext(ResultContext);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './App'
-import './global.css'
+import App from "./App";
+import "./global.css";
+import { ResultContextProvider } from "./Context/ResultContextProvider";
 
 // react-router allows switching between different pages
 ReactDOM.render(
-<Router>
-<App/>
-</Router>, document.getElementById('root'));
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
+  document.getElementById("root")
+);
